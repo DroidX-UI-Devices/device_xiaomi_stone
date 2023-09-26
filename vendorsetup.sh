af8375ff3347aca78bc4126bcdfb77946f0fd42d
Drpto <dr.juniornoob@gmail.com>
2023-09-26 22:47:04 +0530

    stone: Don't track hardware/xiaomi from now
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,6 @@ git clone --depth=1 https://github.com/stone-devs/kernel_xiaomi_sm6375.git -b li
 
 git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
-git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+# git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
 
 git clone --depth=1 https://github.com/Drpto/packages_apps_prebuilts.git packages/apps/prebuilts
