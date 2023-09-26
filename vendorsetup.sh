b0e5994ca27262dc4787174c7eec7a335029c92f
Drpto <dr.juniornoob@gmail.com>
2023-09-26 22:45:34 +0530

    stone: rearrange vendorsetup file from Stone Trees git org
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-git clone --depth=1 https://github.com/stone-devs/device_xiaomi_sm6375-common device/xiaomi/sm6375-common
+git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git device/xiaomi/sm6375-common
 
-git clone --depth=1 https://github.com/stone-devs/proprietary_vendor_xiaomi vendor/xiaomi
+git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git vendor/xiaomi
 
-git clone --depth=1 https://github.com/stone-devs/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/stone-devs/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
 
-git clone --depth=1 https://github.com/stone-devs/device_xiaomi_stone-kernel device/xiaomi/stone-kernel
+git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
 
